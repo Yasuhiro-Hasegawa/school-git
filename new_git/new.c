8115faa25ccee8f5fc7e76102fc2c5e76c6a2586
s1260204 <s1260204@std6dc24.u-aizu.ac.jp>
2019-07-11 18:59:00 +0900

    first ver.

---
+++ new_git/new.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+  int dice1;
+  int dice2;
+  srand(time(NULL));
+  dice1 = rand() % 6+1;
+  dice2 = rand() % 6+1;
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d\n",dice1);
+  printf("Die 2: %d\n",dice2);
+  printf("Total value: %d\n",dice1+dice2);
+}
