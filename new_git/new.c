7202b839919e8060942ef03ae14a8eed93faa729
s1260204 <s1260204@std6dc24.u-aizu.ac.jp>
2019-07-11 19:16:39 +0900

    new ver.

--- new_git/new.c
+++ new_git/new.c
@@ -11,4 +11,6 @@ int main(){
   printf("Die 1: %d\n",dice1);
   printf("Die 2: %d\n",dice2);
   printf("Total value: %d\n",dice1+dice2);
+  if(dice1 + dice2 >= 7)printf("You won\n");
+  else printf("You lost\n");
 }
